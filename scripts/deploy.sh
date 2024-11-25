f6fdf983b805399b3a8805dea57b2595004fffd1
Andreas Belavic <andreasbelavic@gmx.de>
2024-11-25 11:03:52 +0100

    fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd /var/www/html/bachelor-thesis
+cd /var/www/html/bachelor-thesis/
 week_ctr=$(cat presentations/data/week_counter.txt)
 week_ctr=$((week_ctr - 1))
-git add .
+git add presentations
 git commit -m "week $week_ctr"
 git push
 git subtree push --prefix presentations origin gh-pages
