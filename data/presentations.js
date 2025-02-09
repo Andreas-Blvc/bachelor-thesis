023dd54bd68c952e0b5e5c1322de0832831671df
Andreas Belavic <andreas.belavic@proton.me>
2025-02-09 12:34:53 +0100

    week 14

--- data/presentations.js
+++ data/presentations.js
@@ -1,1 +1,1 @@
-const presentations = ['week1','week10','week11','week12','week13','week2','week3','week4','week5','week6','week7','week8','week9'];
+const presentations = ['week1','week2','week3','week4','week5','week6','week7','week8','week9','week10','week11','week12','week13','week14'];
